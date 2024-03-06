dddc467e0f510fe381060cea3c736a4386ae4432
PradnyaBapte <156272495+PradnyaBapte@users.noreply.github.com>
2024-03-06 09:54:15 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,21 +1,50 @@
+
 terraform {
+
   required_version = "~> 1.1"
+
   required_providers {
+
     aws = {
+
       version = "~>3.27"
+
     }
+
   }
+
 }
+
 provider "aws" {
+
   region     = var.region_name
+
   access_key = var.access_key
+
   secret_key = var.secret_key
+
+}
+
+resource "aws_instance" "yourec2" {
+
+  ami                    = "ami-0f403e3180720dd7e "
+
+  instance_type          = "t2.micro"
+
+  tags = {
+
+    Name = "terraform-inst"
+
+  }
+
 }
  
-resource "aws_instance" "yourec2" {
-  ami                    = "ami-0440d3b780d96b29d "
-  instance_type          = lookup(var.instance_type,terraform.workspace)
-  tags = {
-    Name = "terraform-inst"
-  }
+variable "access_key" { }
+
+variable "secret_key" { }
+
+variable "region_name" {
+
+default = "us-east-1"
+
 }
